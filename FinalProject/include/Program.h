703bf7f60d7957d071c3788415ba628adfde34b2
demo9848 <demo9848@colorado.edu>
2015-04-23 20:57:20 -0600

    created HashNode struct and hashTable vector of said structs being private to the class

--- FinalProject/include/Program.h
+++ FinalProject/include/Program.h
@@ -1,6 +1,13 @@
 #ifndef PROGRAM_H
 #define PROGRAM_H
 #include <string>
+#include <vector>
+
+struct HashNode
+{
+    int value = -1;
+    vector<std::string> words;
+};
 
 class Program
 {
@@ -8,17 +15,18 @@ class Program
         Program();
         virtual ~Program();
 
-        void createHashtable(); ///this is where we build the Hashtable
-        void addWord(std::string word); ///use this to add a word to the table
-        int checkSum(std::string x, int s); ///get sum of word ascii values
-        std::string encrypt(std::string x, int key); ///encrypt obviously
-        std::string decrypt(std::string x, int key); ///decrypt obviously..
-        ///What other functions could we include? we need 10
+        void createHashtable(); //this is where we build the Hashtable
+        void addWord(std::string word); //use this to add a word to the table
+        int checkSum(std::string x, int s); //get sum of word ascii values and % by size of table
+        std::string encrypt(std::string x, int key); //encrypt obviously
+        std::string decrypt(std::string x, int key); //decrypt obviously..
 
+        //What other functions could we include? we need 10
 
     protected:
     private:
-        ///need to decide how we will create our hashtable and what type it will be, what size, ect.
+        vector<HashNode> hashTable; //upon creation, increases table to have predefined # of empty HashNodes
+        int tableSize = 0; //defining number for size of hashTable
 };
 
 #endif // PROGRAM_H
