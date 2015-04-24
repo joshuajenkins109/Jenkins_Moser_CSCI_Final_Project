f5db60f5bbb9d3725a34b0428bb76d6794b6a080
joshuajenkins109 <joje3221@colorado.edu>
2015-04-23 19:23:09 -0600

    starting to map out plan for functions

--- FinalProject/include/Program.h
+++ FinalProject/include/Program.h
@@ -1,12 +1,21 @@
 #ifndef PROGRAM_H
 #define PROGRAM_H
-
+#include <string>
 
 class Program
 {
     public:
         Program();
         virtual ~Program();
+
+        void createHashtable(); ///this is where we build the Hashtable
+        void addWord(std::string word); ///use this to add a word to the table
+        int checkSum(std::string x, int s); ///get sum of word ascii values
+        std::string encrypt(std::string x, int key); ///encrypt obviously
+        std::string decrypt(std::string x, int key); ///decrypt obviously..
+        ///What other functions could we include? we need 10
+
+
     protected:
     private:
 };
